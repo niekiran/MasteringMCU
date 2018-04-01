02d5adfd7a6fab8f2a3dbc69f54e80d2b4c6bc76
Kiran Nayak <niekiran@gmail.com>
2018-04-01 11:16:30 +0530

    hal gpio configure bug fix

--- Resources/Source_code/gpio/Driver/hal_gpio_driver.c
+++ Resources/Source_code/gpio/Driver/hal_gpio_driver.c
@@ -10,7 +10,9 @@
 	*/
 void hal_gpio_configure_pin_mode(GPIO_TypeDef *GPIOx, uint16_t pin_no, uint32_t mode)
 {
-	 GPIOx->MODER   |= (mode << ( 2 * pin_no));
+	 
+	GPIOx->MODER   &= ~(0x03 << ( 2 * pin_no));
+	GPIOx->MODER   |= (mode << ( 2 * pin_no));
 }
 
 /**
