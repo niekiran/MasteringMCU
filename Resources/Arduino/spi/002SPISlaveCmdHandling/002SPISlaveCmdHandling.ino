4dcb8fce1a10bfbe1efae3413ee1bf278e133bc9
Amit Alon <amita1974@gmail.com>
2020-09-05 04:34:09 +0300

    Minor improvements on the Arduino 002SPISlaveCmdHandling code
    Addd some prints
    Few code allignments.
    Renamed function checkData to CheckCmdOpcode and implemented the it.

--- Resources/Arduino/spi/002SPISlaveCmdHandling/002SPISlaveCmdHandling.ino
+++ Resources/Arduino/spi/002SPISlaveCmdHandling/002SPISlaveCmdHandling.ino
@@ -19,16 +19,16 @@ uint8_t dataBuff[255];
 
 uint8_t board_id[10] = "ARDUINOUNO";
 
-#define NACK 0xA5
-#define ACK 0xF5
+#define NACK  0xA5
+#define ACK   0xF5
 
 
 //command codes
 #define COMMAND_LED_CTRL          0x50
 #define COMMAND_SENSOR_READ       0x51
 #define COMMAND_LED_READ          0x52
-#define COMMAND_PRINT           0x53
-#define COMMAND_ID_READ         0x54
+#define COMMAND_PRINT             0x53
+#define COMMAND_ID_READ           0x54
 
 #define LED_ON     1
 #define LED_OFF    0
@@ -44,10 +44,10 @@ uint8_t board_id[10] = "ARDUINOUNO";
 void SPI_SlaveInit(void) 
 { 
   // Initialize SPI pins.
-  pinMode(SCK, INPUT);
+  pinMode(SCK,  INPUT);
   pinMode(MOSI, INPUT);
   pinMode(MISO, OUTPUT);
-  pinMode(SS, INPUT);
+  pinMode(SS,   INPUT);
   //make SPI as slave
   
   // Enable SPI as slave.
@@ -72,6 +72,8 @@ void SPI_SlaveTransmit(uint8_t data)
   
   /* Wait for transmission complete */
   while(!(SPSR & (1<<SPIF)));
+  Serial.print("SPISlave sent byte: 0x");
+  Serial.println(data, HEX);
 }
   
 
@@ -93,35 +95,54 @@ void setup()
 }
 
 
-byte checkData(byte commnad)
+byte checkCmdOpcode(byte cmdOpcode)
 {
-  //todo
-  return ACK;
+  Serial.print("command received: 0x");
+  Serial.println(cmdOpcode, HEX);
+  if ((cmdOpcode == COMMAND_LED_CTRL) ||
+      (cmdOpcode == COMMAND_SENSOR_READ) ||
+      (cmdOpcode == COMMAND_LED_READ) ||
+      (cmdOpcode == COMMAND_PRINT) ||
+      (cmdOpcode == COMMAND_ID_READ)) {
+    return ACK;
+  }
+  // Illegal opcode received.
+  return NACK;
 }
 
 // The loop function runs continuously after setup().
 void loop() 
 {
-  byte data,command,len,ackornack=NACK;
+  byte data, cmdOpcode, len, ackornack = NACK;
   
   //1. fist make sure that ss is low . so lets wait until ss is low 
   Serial.println("Slave waiting for ss to go low");
   while(digitalRead(SS) );
+  Serial.println("Slave ss is now low");
   
   //2. now lets wait until rx buffer has a byte
-  command = SPI_SlaveReceive();
-  ackornack = checkData(command);
+  cmdOpcode = SPI_SlaveReceive();
+  ackornack = checkCmdOpcode(cmdOpcode);
   
   SPI_SlaveTransmit(ackornack);
   
   len = SPI_SlaveReceive(); //dummy byte
   
-  if(command == COMMAND_LED_CTRL)
+  if(cmdOpcode == COMMAND_LED_CTRL)
   {
     //read 2 more bytes pin number and value 
     uint8_t pin = SPI_SlaveReceive(); 
     uint8_t value = SPI_SlaveReceive(); 
-    Serial.println("RCVD:COMMAND_LED_CTRL");
+    Serial.print("RCVD:COMMAND_LED_CTRL on pin: ");
+    Serial.print(pin);
+    if (value == 1) {
+      Serial.println(" ON (1)");
+    } else if (value == 0) {
+      Serial.println(" OFF (0)");
+    } else {
+      Serial.print(" -- ERROR: value unexpected: 0x");
+      Serial.println(value, HEX);
+    }
     if(value == (uint8_t)LED_ON)
     {
       digitalWrite(pin,HIGH);
@@ -129,8 +150,7 @@ void loop()
     {
       digitalWrite(pin,LOW);
     }
-  
-  }else if ( command == COMMAND_SENSOR_READ)
+  }else if ( cmdOpcode == COMMAND_SENSOR_READ)
   {
     //read analog pin number 
     uint16_t aread;
@@ -144,11 +164,16 @@ void loop()
   
     val = SPI_SlaveReceive(); //dummy read
     
-    Serial.println("RCVD:COMMAND_SENSOR_READ");
+    Serial.print("RCVD:COMMAND_SENSOR_READ from pin");
+    Serial.print(pin);
+    Serial.print(" Analog port value: ");
+    Serial.print(aread);
+    Serial.print(", ");
+    Serial.println(val);
   
     
   
-  }else if ( command == COMMAND_LED_READ)
+  }else if ( cmdOpcode == COMMAND_LED_READ)
   {
     uint8_t pin = SPI_SlaveReceive(); 
     uint8_t val = digitalRead(pin);
@@ -156,7 +181,7 @@ void loop()
     val = SPI_SlaveReceive(); //dummy read
     Serial.println("RCVD:COMMAND_LED_READ");
   
-  }else if ( command == COMMAND_PRINT)
+  }else if ( cmdOpcode == COMMAND_PRINT)
   {
     uint8_t len = SPI_SlaveReceive(); 
     for(int i=0 ; i < len ; i++)
@@ -167,7 +192,7 @@ void loop()
     
     Serial.println("RCVD:COMMAND_PRINT");
   
-  }else if ( command == COMMAND_ID_READ)
+  }else if ( cmdOpcode == COMMAND_ID_READ)
   {
     for(int i=0 ; i < strlen(board_id) ; i++)
     {
